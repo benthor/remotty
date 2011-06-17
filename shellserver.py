2a0e4be3ea4f4bc68706927700af6e2b7b805d8b
benthor <benthor@os3.nl>
2011-06-17 23:43:14 +0200

    shell client and server implementation demoed

--- shellserver.py
+++ shellserver.py
@@ -6,31 +6,77 @@ import sys
 import fcntl
 import os
 
+BUFFSIZE=512
+TIMEOUT = 1
+
 class ShellServer:
     def __init__(self, socket, shell="/bin/zsh"):
         self.P = Popen([shell,"-i"], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
         self.s = socket
         self.s.setblocking(0)
-        for fd in [sys.stdin.fileno(), P.stdout.fileno()]:
+        # maybe this works for the socket filedescriptor as well XXX
+        for fd in [self.P.stdout.fileno()]:
             fl = fcntl.fcntl(fd, fcntl.F_GETFL)
             fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
 
     def run(self):
+        P = self.P
         while not P.poll():
-            readables, writables, _nothing = select.select([P.stdout, sys.stdin],
-                    [P.stdin, sys.stdout], [], 1)
+            readables, writables, _nothing = select.select([P.stdout,
+                self.s], [P.stdin, self.s], [], TIMEOUT)
             for r in readables:
                 if r == P.stdout:
-                    if sys.stdout in writables:
-                        sys.stdout.write(r.read())
+                    if self.s in writables:
+                        self.s.send(r.read())
                 else:
                     if P.stdin in writables:
-                        P.stdin.write(r.read())
+                        P.stdin.write(r.recv(BUFFSIZE))
 
         return P.returncode
 
-                
+
+class ShellClient:
+    def __init__(self, socket):
+        self.s = socket
+        self.s.setblocking(0)
+        for fd in [sys.stdin.fileno()]:
+            fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+            fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
+    def run(self):
+        while not sys.stdin.closed:
+            readables, writables, _nothing = select.select([sys.stdin, self.s],
+                    [sys.stdout, self.s], [], TIMEOUT)
+            for r in readables:
+                if r == self.s:
+                    if sys.stdout in writables:
+                        sys.stdout.write(r.recv(BUFFSIZE))
+                else:
+                    if self.s in writables:
+                        self.s.send(r.read())
 
 
+import socket
+
+servsoc, clntsoc = socket.socketpair()
+
+def runserver(sock):
+    S = ShellServer(sock)
+    S.run()
+
+def runclient(sock):
+    C = ShellClient(sock)
+    C.run()
+
+from multiprocessing import Process
+
+s = Process(target=runserver, args=(servsoc,))
+s.start()
+
+c = Process(target=runclient, args=(clntsoc,))
+c.start()
+
+s.join()
+c.join()
 
 
