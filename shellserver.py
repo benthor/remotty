ad195e7df9aa3b3707d3f533578ccc855affe5bd
benthor <benthor@os3.nl>
2011-06-18 02:37:04 +0200

    some server tweaks

--- shellserver.py
+++ shellserver.py
@@ -1,6 +1,6 @@
 #import terminal
 from subprocess import Popen, PIPE, STDOUT
-import select
+from select import select
 import sys
 #import tty
 import fcntl
@@ -11,78 +11,47 @@ TIMEOUT = 1
 
 class ShellServer:
     def __init__(self, socket, shell="/bin/zsh"):
-        self.P = Popen([shell,"-i"], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        self.tosocket = os.fdopen(socket.fileno(), "w")
-        self.frsocket = os.fdopen(socket.fileno(), "r")
-        #self.s.setblocking(0)
-        # maybe this works for the socket filedescriptor as well XXX
-        for fd in [self.P.stdout.fileno()]:#, socket.fileno()]:
+        self.P = Popen([shell], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+        self.S = socket
+        self.S.setblocking(0)
+        # FIXME: remove loop 
+        for fd in [self.P.stdout.fileno()]:
             fl = fcntl.fcntl(fd, fcntl.F_GETFL)
             fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
 
-
-
     def run(self):
         P = self.P
+        S = self.S
         while not P.poll():
-            readables = select.select([P.stdout,self.frsocket], [], [],
-                    TIMEOUT)[0]
-            writables = select.select([], [P.stdin, self.tosocket], [],
-                    TIMEOUT)[1]
-            for r in readables:
-                if r == P.stdout:
-                    if self.tosocket in writables:
-                        self.tosocket.write(r.read())
-                        self.tosocket.flush()
+            writables = select([],[S, P.stdin],[],TIMEOUT)[1]
+            for w in writables:
+                if w == P.stdin:
+                    r = select([S],[],[],TIMEOUT)[0]
+                    if r:
+                        s = S.recv(BUFFSIZE)
+                        print s
+                        w.write(s)
+                        w.flush()
                 else:
-                    if P.stdin in writables:
-                        P.stdin.write(r.read())
+                    r = select([P.stdout],[],[],TIMEOUT)[0]
+                    if r:
+                        S.send(r[0].read())
 
         return P.returncode
 
 
-class ShellClient:
-    def __init__(self, socket):
-        self.s = socket
-        #self.s.setblocking(0)
-        for fd in [sys.stdin.fileno()]:
-            fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-            fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-
-    def run(self):
-        while not sys.stdin.closed:
-            readables, writables, _nothing = select.select([sys.stdin, self.s],
-                    [sys.stdout, self.s], [], TIMEOUT)
-            for r in readables:
-                if r == self.s:
-                    if sys.stdout in writables:
-                        sys.stdout.write(r.recv(BUFFSIZE))
-                else:
-                    if self.s in writables:
-                        self.s.send(r.read())
-
-
-import socket
-
-servsoc, clntsoc = socket.socketpair()
-
-def runserver(sock):
-    S = ShellServer(sock)
-    S.run()
-
-def runclient(sock):
-    C = ShellClient(sock)
-    C.run()
-
-from multiprocessing import Process
-
-s = Process(target=runserver, args=(servsoc,))
-s.start()
-
-c = Process(target=runclient, args=(clntsoc,))
-c.start()
-
-s.join()
-c.join()
-
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        import socket
+        s = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
+        try:
+            s.bind('\0'+sys.argv[1])
+            s.listen(1)
+            con = s.accept()[0]
+            S = ShellServer(con)
+            S.run()
+        finally:
+            s.close()     
+    else:
+        sys.stderr.write("usage: python %s <socketname>\n" % sys.argv[0])
 
