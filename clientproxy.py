c36be2c922392d2338e51fa5837a75934d0f36a6
benthor <benthor@os3.nl>
2011-06-19 17:44:28 +0200

    some refactoring

--- clientproxy.py
+++ clientproxy.py
@@ -4,6 +4,9 @@ import sys
 import os
 import fcntl
 
+#Timeout:
+T = None
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         import socket
@@ -17,18 +20,23 @@ if __name__ == "__main__":
             for fd in [sys.stdin, con.fileno()]:
                 fl = fcntl.fcntl(fd, fcntl.F_GETFL)
                 fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-            while not tosocket.closed and not fromsocket.closed:
+            while not tosocket.closed and not fromsocket.closed and not sys.stdin.closed and not sys.stdout.closed:
                 sys.stdin.flush()
                 fromsocket.flush()
-                for readable in select([sys.stdin, fromsocket],[],[])[0]:
+                stuff = select([sys.stdin, fromsocket],[],[sys.stdin,sys.stdout, fromsocket, tosocket],T)
+                if stuff[2]:
+                    raise Exception("some socket issue: %s" % str(stuff[2]))
+                    
+                for readable in stuff[0]:
                     if readable == sys.stdin:
-                        select([],[tosocket],[])[1][0].write(readable.read())
+                        select([],[tosocket],[],T)[1][0].write(readable.read())
                         tosocket.flush()
                     elif readable == fromsocket:
-                        select([],[sys.stdout],[])[1][0].write(readable.read())
+                        select([],[sys.stdout],[],T)[1][0].write(readable.read())
                         sys.stdout.flush()
                     else:
                         raise Exception("Programmer now excepts letterbombs")
+            print "close"
         except:
             raise
         finally:
